7ac85bab8fbbe2b15e89ed18e895011d3dffb27e
Yamzz-Official <ryanrmdn207@gmail.com>
2026-09-19 18:09:45 +0700

    Delete api directory

--- api/admin-login.js
+++
@@ -1,9 +0,0 @@
-const { cookie, json, adminToken } = require('./_db');
-module.exports=async(req,res)=>{
-  if(req.method!=='POST') return json(res,405,{success:false,message:'Method not allowed'});
-  const password=process.env.ADMIN_PASSWORD||'';
-  if(!password) return json(res,500,{success:false,message:'ADMIN_PASSWORD belum diatur'});
-  if((req.body||{}).password!==password) return json(res,401,{success:false,message:'Password admin salah'});
-  cookie(res,'yamzz_admin',adminToken(),86400*7);
-  return json(res,200,{success:true});
-};
